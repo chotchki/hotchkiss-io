9965a68278f51ad0c5c7b89f6ce0eeda97f854ca
Christopher Hotchkiss <christopher.hotchkiss@gmail.com>
2024-11-11 21:49:50 -0800

    Adding docs

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use tracing::info;
 use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
 
+/// Building from here: https://joeymckenzie.tech/blog/templates-with-rust-axum-htmx-askama
 #[tokio::main]
 async fn main() {
     tracing_subscriber::registry()
