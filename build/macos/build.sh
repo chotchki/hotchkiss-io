97f0736f626e9db37779f2cf94b394d9f2a33633
Christopher Hotchkiss <christopher.hotchkiss@gmail.com>
2025-07-19 12:07:22 -0700

    Adding the lilguy approach by @dylanwh, will test on the CI box before I change the build action

---
+++ build/macos/build.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+# Massive thanks to @dylanwh for the approach
+# https://github.com/dylanwh/lilguy/blob/main/macos/build.sh
+set -euo pipefail
+DOMAIN="io.hotchkiss.web"
+EXE="hotchkiss-io"
+OUTPUT="target/apple/release"
+VERSION="0.0.1"
+
+rustup target add aarch64-apple-darwin
+
+cargo build --locked --target aarch64-apple-darwin --release
+
+mkdir -p $OUTPUT
+cp target/release/$EXE $OUTPUT/$EXE
+
+xcrun codesign \
+    --sign "G53N9PU948" \
+    --timestamp \
+    --options runtime \
+    --entitlements macos/entitlements.plist \
+    $OUTPUT/$EXE
+
+pkgbuild --root $OUTPUT \
+    --identifier "$DOMAIN" \
+    --version "$VERSION" \
+    --install-location /Applications \
+    --sign "DG53N9PU948" \
+    target/$EXE.pkg
+
+productbuild \
+    --distribution macos/Distribution.xml \
+    --resources macos/Resources/ --package-path target/ unsigned-$EXE.pkg
+
+productsign --sign "G53N9PU948" unsigned-$EXE.pkg $EXE.pkg
+
+xcrun notarytool submit $EXE.pkg \
+    --keychain-profile "AppPwdNotarizID" \
+    --wait
+
+xcrun stapler staple $EXE.pkg
+
+mv $EXE.pkg "$EXE-$VERSION.pkg"
