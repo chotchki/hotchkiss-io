6770d7ecd1180c42a29fad54aa7f5a6686005de6
Christopher Hotchkiss <christopher.hotchkiss@gmail.com>
2025-07-19 14:25:32 -0700

    typo

--- build/macos/build.sh
+++ build/macos/build.sh
@@ -26,7 +26,7 @@ xcrun codesign \
 
 pkgbuild --root $OUTPUT \
     --identifier "$DOMAIN" \
-    --component-plist build/macos/pkgbuild.plist
+    --component-plist build/macos/pkgbuild.plist \
     --version "$VERSION" \
     --install-location /Applications \
     --sign "G53N9PU948" \
